486796986bf5207f77ecbd997cd6b732ddc58f19
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-09-09 20:05:41 -0300

    fet: Implementando a class de rotas

---
+++ app/helper/Routers.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Helper;
+
+class Routers
+{
+    public function get($uri, $controller):void
+    {
+
+    }
+
+    public function getRouters():array
+    {
+        return [];
+    }
+}
